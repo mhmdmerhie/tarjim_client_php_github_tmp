0371ea7ef38ca1704dd021f5433ec37a3792ed42
Mohammad Merhie <mhmd.merhie@gmail.com>
2022-03-11 12:22:24 +0000

    MOD

--- scripts/initTarjim.php
+++ scripts/initTarjim.php
@@ -1,7 +1,18 @@
 <?php
 
-$file_path = argv[0];
-require_once $file_path;
+$cwd = $argv[1];
+$script_dir = $argv[2];
+$file_name = $argv[3];
+
+echo 'cwd: '.$cwd.PHP_EOL;
+echo 'script_dir: '.$script_dir.PHP_EOL;
+echo 'file_name: '.$file_name.PHP_EOL;
+echo '$cwd$file_name: '.$cwd.'/'.$file_name.PHP_EOL;
+echo 'package_dir: '.$script_dir.'/..'.PHP_EOL;
+
+require_once $cwd.'/'.$file_name;
+
+$package_dir = $script_dir.'/..';
 
 global $cache_dir, $logs_dir, $apikey, $project_id, $default_namespace, $additional_namespaces;
 
@@ -21,16 +32,27 @@ if (empty($logs_dir)) {
 	die("No logs_dir defined");
 }
 
-$config = '
-define(cache_dir, $cache_dir);
-define(logs_dir, $logs_dir);
-define(apikey, $apikey);
-define(project_id, $project_id);
-define(default_namespace, $default_namespace);
-';
+touch($cwd.'/'.$cache_dir.'/translations.json');
+touch($cwd.'/'.$cache_dir.'/translations_backup.json');
+touch($cwd.'/'.$cache_dir.'/sanitized_html.json');
+touch($cwd.'/'.$logs_dir.'/errors.log');
+
+exec('chmod -R 777 '.$cwd.'/'.$cache_dir.' '.$cwd.'/'.$logs_dir); 
+
+$config = "<?php
+define('cache_dir', '{$cwd}/{$cache_dir}');
+define('logs_dir', '{$cwd}/{$logs_dir}');
+define('apikey', '{$apikey}');
+define('project_id', '{$project_id}');
+define('default_namespace', '{$default_namespace}');
+";
 
 if (!empty($additional_namespaces)) {
-	$config .= 'define(additional_namespaces, $additional_namespaces');
+	$config .= 'define(\'additional_namespaces\', '.json_encode($additional_namespaces).');';
+}
+else {
+	$config .= 'define(\'additional_namespaces\', []);';
+
 }
 
-file_put_contents('../src/config.php', $config);
+file_put_contents($package_dir.'/src/config.php', $config);
